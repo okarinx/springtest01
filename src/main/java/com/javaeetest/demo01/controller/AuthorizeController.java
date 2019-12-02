be945f5f6f7273bd22da876e10f686c6dd63edf6
okarinx <xs74678@hotmail.com>
2019-12-03 01:40:21 +0800

    add check cookies

--- src/main/java/com/javaeetest/demo01/controller/AuthorizeController.java
+++ src/main/java/com/javaeetest/demo01/controller/AuthorizeController.java
@@ -17,9 +17,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
-
 
 
 @Controller
@@ -41,7 +42,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -52,14 +54,16 @@ public class AuthorizeController {
         GithubUser githubUser = githubProvider.getUser(accessToken);
         if (githubUser != null) {
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            // 登录成功，写session和cookie
-            request.getSession().setAttribute("user", githubUser);
+            response.addCookie(new Cookie("token", token));
+            // 登录成功，写session和cookie(插入数据库相当于写session)
+            // request.getSession().setAttribute("user", githubUser);
             return "redirect:/"; // 这样地址栏就没有后面的state等信息了
 
         } else {
--- src/main/java/com/javaeetest/demo01/mapper/UserMapper.java
+++ src/main/java/com/javaeetest/demo01/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.javaeetest.demo01.mapper;
 import com.javaeetest.demo01.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,6 @@ import org.springframework.stereotype.Repository;
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
